a509d3b27d5a2f384313e8d27c6bdd981242af57
BERNIER Damione <damione.bernier@SENSN.FR>
2018-10-16 12:05:41 +0200

    maj du pendu

--- Afficheur damione/Pendu/main.cpp
+++ Afficheur damione/Pendu/main.cpp
@@ -2,35 +2,68 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <time.h>
 
 
 using namespace std;
 
-int main()
+std::vector<std::string> lectureFichier()
 {
-    //Le vector permetra de stocker chaque mots
-    //ifstream donne le fichier à lire
-    //string sera le mots a stocker
+    ifstream a("Liste de mots.txt");
+    string b;
+    vector<string>c;
+    //Vérification de la présence du fichier
 
-    vector<string>Liste;
-    ifstream Dico("Liste de mots.txt");
-    string mots;
-
-    //vérification de la présence du fichier
-
-    if(!Dico)
+    if(!a)
     {
         cout<<"Fichier non present"<<endl;
         system("pause");
     }
 
-    //la boucle while lis ligne par ligne tous le fichier et ajoute
+    //La boucle while lis ligne par ligne tous le fichier et ajoute la chaine de caractère trouver à chaque fois.
 
-
-    while(getline(Dico, mots))
+    while(getline(a, b))
     {
-        Liste.push_back(mots);
+        c.push_back(b);
     }
 
-    cout<<Liste[1];
+    return c;
+}
+
+string motAlea(vector<string> a)
+{
+    srand(time(NULL));
+    int random = rand() % a.size();
+
+    string mot = a[random];
+    cout<<random<<"\n";
+    return mot;
+}
+
+//void Affichage()
+//{
+//    for
+//}
+
+int main()
+{
+    //Le vector permetra de stocker chaque mots
+    //ifstream donne le fichier à lire
+    //Le string mot sera le mot a stocker
+
+
+    string motMystere;
+
+    vector<string>Liste = lectureFichier();
+
+
+
+
+    //La méthode motAlea prend le vector en parametre
+    //elle va effectuer le choix du mot aleatoire
+
+    motMystere = motAlea(Liste);
+    cout<<motMystere<<"\n";
+
+
 }
