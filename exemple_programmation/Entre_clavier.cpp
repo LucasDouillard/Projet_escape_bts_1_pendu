fe18cf85df401af4598ed8dffb50e9a53d338d9a
RomainChaillou <43874648+RomainChaillou@users.noreply.github.com>
2018-11-13 09:57:18 +0100

    Update Entre_clavier.cpp

--- exemple_programmation/Entre_clavier.cpp
+++ exemple_programmation/Entre_clavier.cpp
@@ -1,6 +1,12 @@
+#include <conio.h>
+
+
 void AfficheurInterface::Entre_clavier(){
-    char lettre[1];
-    cin.clear();
-    cin >> lettre;
+    char = lettre[1];
+    while( !_kbhit() ) {
+
+    }else{
+        lettre = _kbhit();
+    }
     return lettre;
 }
